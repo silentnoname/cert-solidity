79a7a7abb1728fee6faf3406bbe384e87b224a95
silent666 <635397523pl@gmail.com>
2021-10-17 19:05:43 +0800

    final version

--- cert.sol
+++ cert.sol
@@ -7,7 +7,9 @@ contract cert{
         string course;
         string school;
         uint date;
+        address signer;
         bytes32 certid;
+        bool isUsed;
     }
     
     mapping(address=>cert_details) certificates;
@@ -30,14 +32,23 @@ contract cert{
         return (certificates[sender].name,certificates[sender].course,certificates[sender].school,certificates[sender].date,certificates[sender].certid);
     }
 
-    function viewcertbyid(bytes32 certid) view public returns(string memory name,string memory course,string memory school,uint date){
-        return (certificatesbyid[certid].name,certificatesbyid[certid].course,certificatesbyid[certid].school,certificatesbyid[certid].date);
+    function viewlastcertid(address sender) view public returns(bytes32 certid){
+        return (certificates[sender].certid);
     }
-   
+    function viewcertbyid(bytes32 certid) view public returns(string memory name,string memory course,string memory school,uint date,address singer){
+        return (certificatesbyid[certid].name,certificatesbyid[certid].course,certificatesbyid[certid].school,certificatesbyid[certid].date,certificatesbyid[certid].signer);
+    }
+
+    function verifyid(bytes32 certid) view public returns(bool isUsed) {
+        return certificatesbyid[certid].isUsed;
+    }
+    
     function addcert(string memory name,string memory course,string memory school,uint date) public {
         bytes32 certid;
-        certid=keccak256(abi.encodePacked(name,course,school,date));
-        certificates[msg.sender]=cert_details(name,course,school,date,certid);
-        certificatesbyid[certid]=cert_details(name,course,school,date,certid);
-    }       
+        certid=keccak256(abi.encodePacked(name,course,school,date,msg.sender));
+        bool isUsed=true;
+        certificates[msg.sender]=cert_details(name,course,school,date,msg.sender,certid,isUsed);
+        certificatesbyid[certid]=cert_details(name,course,school,date,msg.sender,certid,isUsed);
+
+    }
  }
